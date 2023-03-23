43ecad6af519736ec03e899b59c1ba20eee6a77b
LazarAlexandru8 <109000947+LazarAlexandru8@users.noreply.github.com>
2023-03-23 11:00:18 +0200

    Update Weather.java

--- WeatherApp/weather/src/main/java/com/lazar/Weather.java
+++ WeatherApp/weather/src/main/java/com/lazar/Weather.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.awt.*;
 import javax.swing.*;
 
+import org.json.JSONObject;
+
 
 
 
@@ -15,33 +17,44 @@ public class Weather extends JFrame {
 
     JLabel gradeC;
     JLabel zona;
+    JButton button;
+    JTextField field;
+    String temperatura;
 
     public void GUI(){
         setTitle("Weather");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
-        setLayout(new GridLayout(2,0));
+        setLayout(new GridLayout(4,0));
         setSize(300,300);
         setLocationRelativeTo(null);
         
         
-        // setExtendedState(java.awt.Frame.);
-        ImageIcon poza = new ImageIcon("poza.jpg");
-        JLabel pozaJ = new JLabel();
-        pozaJ.setIcon(poza);
-        // add(pozaJ);
+
         gradeC = new JLabel("Temperatura in grade C: ");
         add(gradeC);
         zona = new JLabel("Zona: ");
         add(zona);
+        field = new JTextField();
+        add(field);
+        button = new JButton("ENTER");
+        add(button);
+        button.addActionListener((e)-> {
+            String city = field.getText();
+            setZ(city);
+            JSONObject jsonObj = new JSONObject(getWeather(city));
+            Float temp = jsonObj.getJSONObject("current").getFloat("temp_c");
+            String temperatura = temp.toString();
+            setC(temperatura);
 
+        });
         setVisible(true);
     }
 
-    public String getWeather() {
+    public String getWeather(String city) {
         HttpRequest request = HttpRequest.newBuilder()
         // TODO sa generez vremea in functie de oras
-				.uri(URI.create("//API for the weather"))
+				.uri(URI.create("http://api.weatherapi.com/v1/current.json?key=897f96fdadc34d1a8d3231119231503&q="+ city + "&aqi=no"))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
 				.build();
         HttpResponse<String> response = null;
@@ -54,11 +67,11 @@ public class Weather extends JFrame {
     }
 
     public void setC(String x){
-        gradeC.setText(gradeC.getText() + x);
+        gradeC.setText("Temperatura in grade C: " + x);
     }
 
     public void setZ(String y){
-        zona.setText(zona.getText() + y);
+        zona.setText("Zona: " + y);
     }
 
 }
